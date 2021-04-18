a0951da633fbf05607e7f13b62b8d47fa368ee2a
Puneet Tokhi <puneettokhi25@gmail.com>
2021-04-18 00:15:52 -0700

    Implemented the UI

---
+++ WWE_Trivia/WWE_Trivia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WWE_Trivia
+//
+//  Created by Mr. Puneet on 4/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
